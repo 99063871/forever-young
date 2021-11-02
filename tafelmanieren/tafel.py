a2b496107e2411511a48b2d76bc4300c7f74d964
99063871 <99063871@mydavinci.com>
2021-11-02 09:39:13 +0100

    urenprint opnieuw gemaakt en tafel beter gemaakt, nu word de som er ook bij geprint

--- tafelmanieren/tafel.py
+++ tafelmanieren/tafel.py
@@ -1,5 +1,11 @@
 tafel = int(input("Welke tafel wilt u?"))
 
+tafelstr = str(tafel)
+
+
 for x in range(1,11):
+    xstr = str(x)
     a = (x*tafel)
-    print(x*tafel)
+    b = str(a)
+    print(xstr + " * " + tafelstr + " = "+ b)
+    
--- tafelmanieren/urenprint.py
+++ tafelmanieren/urenprint.py
@@ -1,10 +1,8 @@
-import datetime
-now = datetime.datetime.now()
+#Print de hele uren van de dag. Voor de ochtend voeg je 'AM' toe. Voor de middag voeg je 'PM' toe
 
-nowHour = int(now.hour)
-nowHourTxt = str(now.hour)
-if nowHour < 12:
-    print(nowHourTxt + "AM")
-
-elif nowHour >= 12:
-    print(nowHourTxt + "PM")
+for x in range(1,25):
+    xstr = str(x)
+    if x < 12:
+        print(xstr + "AM")
+    else:
+        print(xstr + "PM")
